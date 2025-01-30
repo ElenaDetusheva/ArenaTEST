8944ab2f4d5e3d74b2777d535e7356aa4f97e13e
ElenaDetusheva <e.det@list.ru>
2025-01-30 14:08:16 +0300

    add parametrization

--- cypress/e2e/ARENA/formTesting.spec.cy.js
+++ cypress/e2e/ARENA/formTesting.spec.cy.js
@@ -3,8 +3,7 @@ import happyPass from "../../fixtures/happyPass.json";
 describe('Form testing', ()=>{
 
      it('Use valid data for completing the form ', ()=>{
-      const goals = ['Заказ','Вопрос о товаре','Вопрос в тех.поддержку','Отзыв','Жалоба','Другое']
-        cy.visit('https://kontaktnaya-forma.testograf.ru')
+            cy.visit('https://kontaktnaya-forma.testograf.ru')
       
         cy.get('header div.title').should('be.visible')
                   
@@ -59,6 +58,66 @@ describe('Form testing', ()=>{
            .and('not.contain','E-mail:')
            .and('not.contain','Телефон:')
        }) 
+
+    happyPass.goalOptionsData.forEach((goal,ind) => {
+   
+    it(`Проверка отправки формы с валидными данными и целью обращения "${goal}"`, ()=>{
+          cy.visit('https://kontaktnaya-forma.testograf.ru')
+      
+        cy.get('header div.title').should('be.visible')
+                  
+        cy.get('.shortText input')
+          .eq(0)
+          .type(happyPass.name)
+         
+        cy.get('.shortText input')
+          .should('have.value',happyPass.name)
+
+        cy.get('.shortText input')
+          .eq(1)
+          .type(happyPass.email)
+
+        cy.get('.shortText input')
+          .eq(1)
+          .should('have.value',happyPass.email)
+
+        cy.get('.shortText input')
+          .eq(2)
+          .type(happyPass.phone)
+
+        cy.get('.shortText input')
+          .eq(2)
+          .should('have.value',happyPass.phone)
+        
+        cy.get('#downshift-0-toggle-button>svg').click() 
+        cy.get('.content .option')
+          .then(($els)=> {
+              const goalOptions = Cypress.$.makeArray($els).map(($el)=>$el.textContent)
+              expect(goalOptions).to.be.deep.equal(happyPass.goalOptionsData)
+            })
+
+        cy.get('.content .option')
+          .eq(ind)
+          .click()
+        cy.get('form input').should('have.value',happyPass.goalOptionsData[ind])
+       
+        cy.get('.input textarea')
+          .type(happyPass.message)
+
+        cy.get('.input textarea')
+          .should('have.value',happyPass.message)
+
+        cy.get('button').contains('Отправить').click()
+
+        cy.contains('Благодарим за обращение!').should('be.visible')
+        cy.get('.content ').should('have.text','-\nБлагодарим за обращение!\n')
+
+        cy.get('body')
+           .should('not.contain','Ваше имя:')
+           .and('not.contain','E-mail:')
+           .and('not.contain','Телефон:')
+       })
+  })
   
 })
 
